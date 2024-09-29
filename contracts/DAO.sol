c5d050ed36e0c74dfc18db6a37bd5ef07592f437
lim-haru <123555071+lim-haru@users.noreply.github.com>
2024-09-29 14:58:12 +0200

    Added Delegate Function

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -25,7 +25,10 @@ contract DAO is Ownable {
 
   mapping(address => uint256) public shares;
   mapping(uint256 => Proposal) public proposals;
-  
+  mapping(address => address) public delegate;
+  mapping(address => address[]) public delegator;
+  mapping(address => uint256) public delegatedVotes;
+
   modifier onlyMember() {
     require(shares[msg.sender] > 0, "Not a DAO member");
     _;
@@ -38,7 +41,7 @@ contract DAO is Ownable {
   
   function buyShares(uint256 amount) external {
     require(saleActive, "Token sale is closed");
-    require(token.transferFrom(msg.sender, address(this), amount * pricePerShare), "Token trasfer failed");
+    require(token.transferFrom(msg.sender, address(this), amount * pricePerShare), "Token transfer failed");
 
     shares[msg.sender] += amount;
   }
@@ -57,14 +60,29 @@ contract DAO is Ownable {
     proposal.endTime = block.timestamp + _votingPeriod * 1 days;
   }
 
+  function delegateVote(address to) external onlyMember {
+    require(to != msg.sender, "Cannot delegate to yourself");
+    require(shares[to] > 0, "Delegate must be a member");
+    require(delegate[msg.sender] == address(0), "Already delegated");
+
+    delegate[msg.sender] = to;
+    delegator[to].push(msg.sender);
+    delegatedVotes[to] += shares[msg.sender];
+  }
+
   function vote(uint256 proposalId, uint8 voteOption) external onlyMember {
     Proposal storage proposal = proposals[proposalId];
     require(proposal.endTime > block.timestamp, "Voting period id over");
     require(!proposal.voted[msg.sender], "Already voted");
+    require(delegate[msg.sender] == address(0), "You have delegated your vote, cannot vote directly");
 
-    uint256 voterShares = shares[msg.sender];
+    uint256 voterShares = shares[msg.sender] + delegatedVotes[msg.sender];
 
     proposal.voted[msg.sender] = true;
+    // We mark as "voted" those who have delegated their vote
+    for (uint256 i = 0; i < delegator[msg.sender].length; i++) {
+      proposal.voted[delegator[msg.sender][i]] = true;
+    }
     
     if (voteOption == 1) {
       proposal.votesFor += voterShares;
@@ -89,5 +107,25 @@ contract DAO is Ownable {
         require(token.transfer(proposal.recipient, proposal.amount), "Token transfer failed");
       }
     }
+  }
+
+  function revokeDelegation() external onlyMember {
+    address currentDelegate = delegate[msg.sender];
+    require(currentDelegate != address(0), "No active delegation");
+
+    delegatedVotes[currentDelegate] -= shares[msg.sender];
+    delete delegate[msg.sender];
+    for (uint256 i = 0; i < delegator[currentDelegate].length; i++) {
+      if (delegator[currentDelegate][i] == msg.sender) {
+        delete delegator[currentDelegate][i];
+    } }
+  }
+
+  function hasVoted(uint proposalId, address voter) external view returns (bool) {
+    return proposals[proposalId].voted[voter];
+  }
+
+  function getDelegator(address _delegator) public view  returns (address[] memory) {
+    return delegator[_delegator];
   }
 }
