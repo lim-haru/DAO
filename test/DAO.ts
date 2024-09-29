597a5ee22a5e1c276f6b6ab835f1992d18e5287d
lim-haru <123555071+lim-haru@users.noreply.github.com>
2024-09-29 14:58:53 +0200

    Added Test for Delegate Function

--- test/DAO.ts
+++ test/DAO.ts
@@ -5,7 +5,7 @@ import { loadFixture, time } from "@nomicfoundation/hardhat-toolbox/network-help
 
 describe("DAO", function () {
   async function deploy() {
-    const [owner, addr1, addr2] = await hre.ethers.getSigners();
+    const [owner, addr1, addr2, addr3] = await hre.ethers.getSigners();
     const ONE_DAY_IN_SECONDS = 24 * 60 * 60;
 
     const TokenERC20 = await hre.ethers.getContractFactory("TokenERC20");
@@ -15,8 +15,9 @@ describe("DAO", function () {
     const DAO = await hre.ethers.getContractFactory("DAO");
     const dao = await DAO.deploy(token.getAddress(), pricePerShare);
 
-    return { token, dao, owner, addr1, addr2, ONE_DAY_IN_SECONDS };
+    return { token, dao, owner, addr1, addr2, addr3, ONE_DAY_IN_SECONDS };
   }
+
   it("Should allow users to buy shares and become new members", async function () {
     const { token, dao, addr1 } = await loadFixture(deploy);
 
@@ -80,5 +81,81 @@ describe("DAO", function () {
     const { dao, addr1 } = await loadFixture(deploy);
 
     await expect(dao.connect(addr1).vote(0, 1)).to.be.revertedWith("Not a DAO member");
+  });
+
+  it("Should allow the delegation of votes", async function () {
+    const { token, dao, addr1, addr2 } = await loadFixture(deploy);
+
+    await token.transfer(addr1.address, parseEther("1000.0"));
+    await token.transfer(addr2.address, parseEther("750.0"));
+    await token.connect(addr1).approve(dao.getAddress(), parseEther("1000"));
+    await token.connect(addr2).approve(dao.getAddress(), parseEther("750"));
+    await dao.connect(addr1).buyShares(parseEther("100.0"));
+    await dao.connect(addr2).buyShares(parseEther("75.0"));
+
+    await dao.connect(addr1).delegateVote(addr2.address);
+
+    expect(await dao.delegatedVotes(addr2)).to.equal(parseEther("100.0"));
+  });
+
+  it("Should allow the delegation of votes to be revoked", async function () {
+    const { token, dao, addr1, addr2 } = await loadFixture(deploy);
+
+    await token.transfer(addr1.address, parseEther("1000.0"));
+    await token.transfer(addr2.address, parseEther("750.0"));
+    await token.connect(addr1).approve(dao.getAddress(), parseEther("1000"));
+    await token.connect(addr2).approve(dao.getAddress(), parseEther("750"));
+    await dao.connect(addr1).buyShares(parseEther("100.0"));
+    await dao.connect(addr2).buyShares(parseEther("75.0"));
+    await dao.connect(addr1).delegateVote(addr2.address);
+
+    await dao.connect(addr1).revokeDelegation();
+
+    expect(await dao.delegatedVotes(addr2)).to.equal(0);
+  });
+
+  it("When the delegator votes, they should also mark their delegates as having voted.", async function () {
+    const { token, dao, addr1, addr2, addr3 } = await loadFixture(deploy);
+
+    await token.transfer(addr1.address, parseEther("1000.0"));
+    await token.transfer(addr2.address, parseEther("750.0"));
+    await token.transfer(addr3.address, parseEther("750.0"));
+    await token.connect(addr1).approve(dao.getAddress(), parseEther("1000"));
+    await token.connect(addr2).approve(dao.getAddress(), parseEther("750"));
+    await token.connect(addr3).approve(dao.getAddress(), parseEther("750"));
+    await dao.connect(addr1).buyShares(parseEther("100.0"));
+    await dao.connect(addr2).buyShares(parseEther("75.0"));
+    await dao.connect(addr3).buyShares(parseEther("75.0"));
+    await dao.connect(addr1).delegateVote(addr3.address);
+    await dao.connect(addr2).delegateVote(addr3.address);
+    await dao.connect(addr1).proposeDecision("Proposal 1", "Proposal test 1", hre.ethers.ZeroAddress, 0, 3);
+
+    await dao.connect(addr3).vote(0, 1);
+    expect(await dao.hasVoted(0, addr1)).to.equal(true);
+    expect(await dao.hasVoted(0, addr2)).to.equal(true);
+  });
+
+  it("Should remove all delegates from the delegator registry during delegation revocation.", async function () {
+    const { token, dao, addr1, addr2, addr3 } = await loadFixture(deploy);
+
+    await token.transfer(addr1.address, parseEther("1000.0"));
+    await token.transfer(addr2.address, parseEther("750.0"));
+    await token.transfer(addr3.address, parseEther("750.0"));
+    await token.connect(addr1).approve(dao.getAddress(), parseEther("1000"));
+    await token.connect(addr2).approve(dao.getAddress(), parseEther("750"));
+    await token.connect(addr3).approve(dao.getAddress(), parseEther("750"));
+    await dao.connect(addr1).buyShares(parseEther("100.0"));
+    await dao.connect(addr2).buyShares(parseEther("75.0"));
+    await dao.connect(addr3).buyShares(parseEther("75.0"));
+    await dao.connect(addr1).delegateVote(addr3.address);
+    await dao.connect(addr2).delegateVote(addr3.address);
+    await dao.connect(addr1).proposeDecision("Proposal 1", "Proposal test 1", hre.ethers.ZeroAddress, 0, 3);
+
+    await dao.connect(addr1).revokeDelegation();
+    await dao.connect(addr2).revokeDelegation();
+
+    const delegator = await dao.getDelegator(addr3);
+    expect(delegator[0]).to.equal(hre.ethers.ZeroAddress);
+    expect(delegator[1]).to.equal(hre.ethers.ZeroAddress);
   });
 });
